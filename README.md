657734b9127c6682fbe01d8707d8be6746a2d051
Shata Cheruvu <shatacheruvu@autonxt.in>
2017-10-08 12:20:34 +0530

    Update #2
    Added os module to prevent absolute path errors. Added README and minor bug fixes

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # FaceMaster
+Avoid the hassle of Biometric scanners or manually register entries. FaceMaster smartly records employee attendance everyday. It uses TensorFlow as its learning engine and OpenCV
+to detect human faces based on a pre-trained face detection model.
+
+This project is still in its **beta** phase and will mature overtime with lot more extensions for different platforms.
+
+**Languages Supported:** Python3
+
+#Instructions
+Keep checking this space for instructions.
--- python/register.py
+++ python/register.py
@@ -12,38 +12,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-''' Attendance System based on Transfer Learning on Google's Inception V3 model
-    Train your model based on the instructions given here:
+''' Attendance System based on Transfer Learning on Google's Inception V3 labels
+    Train your labels based on the instructions given here:
     https://www.tensorflow.org/tutorials/image_retraining
     
     This script loads the graph 'output_graph.pb' and feeds in the camera stream
     The region of interest i.e the user's face is extracted from the frame and
     converted to a numpy array. This array is passed to the softmax_tensor as
     input which calculates the predictions from the final layer softmax function.
-    
-    To run this code:
-    
-    python3 register.py
 '''
 
-
 import cv2 as opencv
+import os
 import numpy as np
 import tensorflow as tf
 
 
 def get_labels():
-
-    with open('output_labels.txt', 'r') as file:
+    with open(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'labels/output_labels.txt')), 'r') as file:
         labels = [line.rstrip('\n') for line in file]
     return labels
+
 
 labels = get_labels()
 
 
-def run_detection(imageArray, labels):
-
-    with tf.gfile.FastGFile('output_graph.pb', 'rb') as graph_file:
+def run_detection(image_array, labels):
+    with tf.gfile.FastGFile(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'classifiers/output_graph.pb')),
+                            'rb') as graph_file:
         graph_def = tf.GraphDef()
         graph_def.ParseFromString(graph_file.read())
         _ = tf.import_graph_def(graph_def=graph_def, name='')
@@ -51,7 +47,7 @@ def run_detection(imageArray, labels):
     with tf.Session() as sess:
         softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
 
-        predictions = sess.run(softmax_tensor, {'DecodeJpeg:0': imageArray})
+        predictions = sess.run(softmax_tensor, {'DecodeJpeg:0': image_array})
         prediction = predictions[0]
 
         prediction = prediction.tolist()
@@ -61,7 +57,8 @@ def run_detection(imageArray, labels):
         print("%s (%.2f%%)" % (predicted_person, max_value * 100))
 
 
-face_cascade = opencv.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_cascade = opencv.CascadeClassifier(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'classifiers/haarcascade_frontalface_default.xml')))
 video_cam = opencv.VideoCapture(0)
 while True:
     status, frame = video_cam.read()
@@ -69,4 +66,4 @@ while True:
     for (x, y, width, height) in body:
         region_of_interest = frame[y:y + height, x:x + width]
         if height > 100 or width > 100:
-            run_detection(np.asarray(region_of_interest),labels)
+            run_detection(np.asarray(region_of_interest), labels)
